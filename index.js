66ef64c16b70a789d204ca6cb87a330669c67bb0
Kaarel <kaarel.viilvere@voco.ee>
2024-10-03 14:35:20 +0300

    Create project basic files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require('path');
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'view'))
+app.set('views', path.join(__dirname, 'views'))
     
 app.get('/' ,(req, res) => {
     res.render('index')
