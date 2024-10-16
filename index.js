294b7ed85d143a5bc68478aa0e992f11f75fe33a
Kaarel <kaarel.viilvere@voco.ee>
2024-10-16 10:45:52 +0300

    Read data from file

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   // get tasks from file
-fs.readFile('./tasks', 'utf8', (err,data) =>{
-    if(err){
-        console.error(err);
-        return;
-    } 
-    console.log(data);
-    console.log(typeof data);
-    console.log(data.split("\n"));
-} );
-  // tasks list data
-  const tasks = ["Study HTML", "Study CSS", "Study JS", "Study OOP"];
-  res.render("index", { tasks: tasks });
+  fs.readFile('./tasks', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const tasks = data.split("\n")
+    res.render("index", { tasks: tasks });
+  });
+});
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+  console.log('form sent data');
+  console.log(req.body);
 });
 
 app.listen(3001, () => {
